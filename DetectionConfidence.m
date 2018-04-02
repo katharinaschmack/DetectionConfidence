926fb443856a3e42787f880f050ea2c4ba41bb87
DESKTOP-28AAB8N\Katharina <schmack@cshl.edu>
2018-04-02 17:29:32 -0400

    add PreStimulus State, first version of Main Plot with trial history, Psychometric Function, Trial Rate

--- DetectionConfidence.m
+++ DetectionConfidence.m
@@ -17,7 +17,7 @@ if isempty(fieldnames(TaskParameters))
     TaskParameters.GUIPanels.General = {'Ports_LMR','AfterTrialInterval','AfterTrialIntervalJitter'};
     
     %"stimulus
-    TaskParameters.GUI.PlayStimulus = 3; %stimulus to be played at center port
+    TaskParameters.GUI.PlayStimulus = 4; %stimulus to be played at center port
     TaskParameters.GUIMeta.PlayStimulus.Style = 'popupmenu';
     TaskParameters.GUIMeta.PlayStimulus.String = {'None','Noise','Noise+Signal (easy)','Noise+Signal (variable)'};% 
     TaskParameters.GUI.MinStimDuration = 0.05;%minimum sample time required for reward will not decrease further (0.05 in Marion's script)
@@ -28,36 +28,58 @@ if isempty(fieldnames(TaskParameters))
     TaskParameters.GUIMeta.AutoRampStimDuration.Style = 'checkbox';
     TaskParameters.GUI.StimDurationRampUp = 0.01;
     TaskParameters.GUI.StimDurationRampDown = 0.005;        
-    TaskParameters.GUI.PostStimDuration = .05;
+    TaskParameters.GUI.PreStimDuration = 0;
+    TaskParameters.GUI.PostStimDuration = 0;
     TaskParameters.GUI.RewardAmountCenter = .5;%reward amount center ports (marion .5)
     TaskParameters.GUI.CoutEarlyTimeout = 1;%time out for early withdrawal (marion 1s)
 
     %TaskParameters.GUI.EarlyWithdrawalNoise = true;%punishing sound for early withdrawal (marion true)
     %TaskParameters.GUIMeta.EarlyWithdrawalNoise.Style='checkbox';
-    TaskParameters.GUIPanels.Stimulus = {'PlayStimulus','StimDuration','PostStimDuration','RewardAmountCenter','CoutEarlyTimeout',...
+    TaskParameters.GUIPanels.Stimulus = {'PlayStimulus','PreStimDuration','StimDuration','PostStimDuration','RewardAmountCenter','CoutEarlyTimeout',...
         'AutoRampStimDuration','MinStimDuration','MaxStimDuration','StimDurationRampUp','StimDurationRampDown'};
     
     %Reward & Punishment
     TaskParameters.GUI.ChoiceDeadline = 5; %Maximal Interval for choice after stimulus presentAfterTrialIntervalon
     TaskParameters.GUI.RewardAmountCorrect = 2;%reward amount lateral ports (marion 5)
-    TaskParameters.GUI.RewardAmountError = 0;%reward amount lateral ports (marion 5)
+    TaskParameters.GUI.RewardAmountError = 2;%reward amount lateral ports (marion 5)
     TaskParameters.GUI.GracePeriod = 0;%grace period
     %TaskParameters.GUI.ConfidenceWaitingTime = 1;
     TaskParameters.GUI.ErrorTimeout = 1;%time out for errors 
-    TaskParameters.GUI.LightGuidance = 1;%proportion of trials with light guidance to correct port    TaskParameters.GUI.AutoRampLightGuidance = false; %for training    
+    TaskParameters.GUI.MaxLightGuidance = 1;%proportion of trials with light guidance to correct port    TaskParameters.GUI.AutoRampLightGuidance = false; %for training    
+    TaskParameters.GUI.MinLightGuidance = 1;%proportion of trials with light guidance to correct port    TaskParameters.GUI.AutoRampLightGuidance = false; %for training
+    TaskParameters.GUI.LightGuidance = TaskParameters.GUI.MaxLightGuidance;
+    TaskParameters.GUIMeta.LightGuidance.Style = 'text';
+
     TaskParameters.GUI.AutoRampLightGuidance = true;
     TaskParameters.GUIMeta.AutoRampLightGuidance.Style = 'checkbox';
     TaskParameters.GUI.LightGuidanceRampDown = .1; %for training
     TaskParameters.GUI.LightGuidanceRampUp = .1; %for training
-    TaskParameters.GUIPanels.Reward = {'ChoiceDeadline','RewardAmountCorrect','RewardAmountError','GracePeriod','ErrorTimeout','LightGuidance','AutoRampLightGuidance','LightGuidanceRampDown','LightGuidanceRampUp',};%,'Deplete','DepleteRate','Jackpot','JackpotMin','JackpotTime'};
+    TaskParameters.GUIPanels.Reward = {'ChoiceDeadline','RewardAmountCorrect','RewardAmountError','GracePeriod','ErrorTimeout','LightGuidance','AutoRampLightGuidance','MaxLightGuidance','MinLightGuidance','LightGuidanceRampDown','LightGuidanceRampUp',};%,'Deplete','DepleteRate','Jackpot','JackpotMin','JackpotTime'};
 
+    %Plot
+    TaskParameters.GUI.ShowPsycAud = 1;
+    TaskParameters.GUIMeta.ShowPsycAud.Style = 'checkbox';
+    TaskParameters.GUI.ShowVevaiometric = 1;
+    TaskParameters.GUIMeta.ShowVevaiometric.Style = 'checkbox';
+    TaskParameters.GUI.ShowTrialRate = 1;
+    TaskParameters.GUIMeta.ShowTrialRate.Style = 'checkbox';
+    TaskParameters.GUI.ShowFix = 1;
+    TaskParameters.GUIMeta.ShowFix.Style = 'checkbox';
+    TaskParameters.GUI.ShowST = 1;
+    TaskParameters.GUIMeta.ShowST.Style = 'checkbox';
+    TaskParameters.GUI.ShowFeedback = 1;
+    TaskParameters.GUIMeta.ShowFeedback.Style = 'checkbox';
+    TaskParameters.GUIPanels.ShowPlots = {'ShowPsycAud','ShowVevaiometric','ShowTrialRate','ShowFix','ShowST','ShowFeedback'};
+    
     TaskParameters.GUI = orderfields(TaskParameters.GUI);
     TaskParameters.Figures.OutcomePlot.Position = [200, 200, 1000, 400];
 end
 BpodParameterGUI('init', TaskParameters);
 
 %put trial-by-trial varying settings into BpodSystem.Data.Custom
+BpodSystem.Data.Custom.PreStimDuration = TaskParameters.GUI.PreStimDuration;
 BpodSystem.Data.Custom.StimDuration = TaskParameters.GUI.StimDuration;
+
 BpodSystem.Data.Custom.ConfidenceWaitingTime = 0;%update here for confidence report training
 if TaskParameters.GUI.AfterTrialIntervalJitter
     BpodSystem.Data.Custom.AfterTrialInterval = min( [ exprnd(TaskParameters.GUI.AfterTrialInterval) 5*TaskParameters.GUI.AfterTrialInterval ]);
@@ -67,7 +89,10 @@ end
 BpodSystem.Data.Custom.RewardAmountCorrect(1) = TaskParameters.GUI.RewardAmountCorrect;
 BpodSystem.Data.Custom.RewardAmountCenter(1) = TaskParameters.GUI.RewardAmountCenter;
 BpodSystem.Data.Custom.RewardAmountError(1) = TaskParameters.GUI.RewardAmountError;
-BpodSystem.Data.Custom.LightGuidance = TaskParameters.GUI.LightGuidance;
+
+
+BpodSystem.Data.Custom.LightGuidance = TaskParameters.GUI.MaxLightGuidance;
+BpodSystem.Data.Custom.ErrorPortLightIntensity = ceil(255* (rand >= TaskParameters.GUI.LightGuidance));%set LED intensity to 0 on error port on some trials for training
 
 %server data
 [~,BpodSystem.Data.Custom.Rig] = system('hostname');
@@ -84,7 +109,7 @@ if TaskParameters.GUI.PlayStimulus>1
     StimulusSettings.SamplingRate=192000;%sampling rate of sound card
     StimulusSettings.NoiseDuration=BpodSystem.Data.Custom.StimDuration;
     StimulusSettings.NoiseColor='WhiteGaussian';
-    StimulusSettings.NoiseVolume=40;%in dB
+    StimulusSettings.NoiseVolume=50;%in dB
     StimulusSettings.SignalForm='QuadraticConvex';
     StimulusSettings.SignalMinFreq=5E3;
     StimulusSettings.SignalMaxFreq=10E3;
@@ -96,7 +121,7 @@ if TaskParameters.GUI.PlayStimulus>1
     elseif TaskParameters.GUI.PlayStimulus == 3 %noie plus easy signals
         StimulusSettings.EmbedSignal=(fix(rand*2));
         StimulusSettings.SignalDuration=min([0.1 BpodSystem.Data.Custom.StimDuration]);%plays signal of 0.1 s or sample time duration (if shorter)
-        StimulusSettings.SignalVolume=40;%in dB
+        StimulusSettings.SignalVolume=StimulusSettings.EmbedSignal*40;%in dB
     elseif TaskParameters.GUI.PlayStimulus == 4 %noise plus easy and difficult signals
         StimulusSettings.EmbedSignal=(fix(rand*5))/4;%for 5 signal intensities between 0(noise) and 1 (signal)
         StimulusSettings.SignalDuration=min([0.1 BpodSystem.Data.Custom.StimDuration]);%plays signal of 0.1 s or sample time duration (if shorter)
@@ -140,7 +165,18 @@ BpodSystem.GUIHandles.OutcomePlot.HandleGracePeriod = axes('Position',  [1*.05  
 BpodSystem.GUIHandles.OutcomePlot.HandleTrialRate = axes('Position',    [3*.05 + 2*.08   .6  .1  .3], 'Visible', 'off');
 BpodSystem.GUIHandles.OutcomePlot.HandleST = axes('Position',           [5*.05 + 4*.08   .6  .1  .3], 'Visible', 'off');
 BpodSystem.GUIHandles.OutcomePlot.HandleMT = axes('Position',           [6*.05 + 6*.08   .6  .1  .3], 'Visible', 'off');
-%MouseNosePoke_PlotSideOutcome(BpodSystem.GUIHandles.OutcomePlot,'init');
+
+BpodSystem.ProtocolFigures.SideOutcomePlotFig = figure('Position', TaskParameters.Figures.OutcomePlot.Position,'name','Outcome plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleOutcome = axes('Position',    [  .055          .15 .91 .3]);
+BpodSystem.GUIHandles.OutcomePlot.HandlePsycAud = axes('Position',    [2*.05 + 1*.08   .6  .1  .3], 'Visible', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleTrialRate = axes('Position',  [3*.05 + 2*.08   .6  .1  .3], 'Visible', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleFix = axes('Position',        [4*.05 + 3*.08   .6  .1  .3], 'Visible', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleST = axes('Position',         [5*.05 + 4*.08   .6  .1  .3], 'Visible', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleFeedback = axes('Position',   [6*.05 + 5*.08   .6  .1  .3], 'Visible', 'off');
+BpodSystem.GUIHandles.OutcomePlot.HandleVevaiometric = axes('Position',   [7*.05 + 6*.08   .6  .1  .3], 'Visible', 'off');
+MainPlot(BpodSystem.GUIHandles.OutcomePlot,'init');
+%BpodSystem.ProtocolFigures.ParameterGUI.Position = TaskParameters.Figures.ParameterGUI.Position;
+
 
 %% Main loop
 RunSession = true;
@@ -167,7 +203,7 @@ while RunSession
     end
     
     updateCustomDataFields(iTrial)%get data and create new stimuli here
-    %MouseNosePoke_PlotSideOutcome(BpodSystem.GUIHandles.OutcomePlot,'update',iTrial);
+    MainPlot(BpodSystem.GUIHandles.OutcomePlot,'update',iTrial);
     iTrial = iTrial + 1;
 end
 
