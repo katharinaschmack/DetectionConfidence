1a8d0a231edaac27f3837f02498f90b376e06fb9
rig-02\Katharina <schmack@cshl.edu>
2018-04-09 17:33:01 -0400

    no message

--- GenerateSignalInNoiseStimulus.m
+++ GenerateSignalInNoiseStimulus.m
@@ -51,7 +51,7 @@ if(isempty(SoundCal))
 end
 toneAtt = mean(polyval(SoundCal(1,1).Coefficient,linspace(SignalMinFreq,SignalMaxFreq))); %just take the mean over signal frequencies -
 %toneAtt = [polyval(SoundCal(1,1).Coefficient,toneFreq)' polyval(SoundCal(1,2).Coefficient,toneFreq)']; in Torben's script
-diffSPL = NoiseVolume - [SoundCal.TargetSPL];
+diffSPL = NoiseVolume - [SoundCal(1,1).TargetSPL];
 attFactor = sqrt(10.^(diffSPL./10)); %sqrt(10.^(diffSPL./10)) in Torben's script WHY sqrt?
 att = toneAtt.*attFactor;%this is the value for multiplying signal scaled/clipped to [-1 to 1]
 noise=noise*att;
