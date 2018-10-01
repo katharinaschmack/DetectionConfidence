28f45719b17d53b37ba198249c72d2bc3fe06ce4
DESKTOP-28AAB8N\Katharina <schmack@cshl.edu>
2018-09-30 22:38:52 -0400

    new figure

--- UserKillScriptKatharina.m
+++ UserKillScriptKatharina.m
@@ -20,23 +20,29 @@ catch
     fprintf('Noise stream not stopped.')
 end
 
-%% save figures
-FigureFolder = fullfile(fileparts(fileparts(BpodSystem.DataPath)),'Session Figures');
-[~, FigureName] = fileparts(BpodSystem.DataPath);
-if ~isdir(FigureFolder)
-    mkdir(FigureFolder);
-end
 
 % Export Online Figure
-try
-    saveas(BpodSystem.ProtocolFigures.SideOutcomePlotFig,fullfile(FigureFolder,FigureName),'png')
-end
+% try
+%     saveas(BpodSystem.ProtocolFigures.SideOutcomePlotFig,fullfile(FigureFolder,FigureName),'png')
+% end
 
 % Create Analysis Figure and Data
 [~,TitleString] = fileparts(BpodSystem.DataPath);
-if BpodSystem.Data.nTrials>1
-     [trialTab sessionTab]=retrieveDataOnline(SessionData);
-   [FigureHandle,infostring]=AnalysisFigureOnline(trialTab,sessionTab,TitleString);
+try
+    %% save figures
+    FigureFolder = fullfile(fileparts(fileparts(BpodSystem.DataPath)),'Session Figures');
+    [~, FigureName] = fileparts(BpodSystem.DataPath);
+    if ~isdir(FigureFolder)
+        mkdir(FigureFolder);
+    end
+
+    [trialTab,sessionTab]=retrieveDataOnline(SessionData);
+    metadata=defineMetadata();
+    metadata.titlestring=TitleString;
+    metadata.pooled=true;
+    metadata.history='correct';
+    [dayTab]=retrieveDaydata(sessionTab,trialTab,metadata.binning.noise);
+    [FigureHandle,infostring]=AnalysisFigure(trialTab,sessionTab,dayTab,metadata);
     
     expression = '\w\w\w\d\d_2018*';
     matchStr = regexp(FigureName,expression,'match');
@@ -58,7 +64,7 @@ if BpodSystem.Data.nTrials>1
         titstr=strsplit(TitleString,'_');
         note = sprintf('%s %s %s:',titstr{3},titstr{4},titstr{5});%date year session
         for k=1:length(infostring)
-        note = sprintf('%s\n%s',note,infostring{k});
+            note = sprintf('%s\n%s',note,infostring{k});
         end
         note = sprintf('%s\n\n',note);
         
@@ -71,46 +77,68 @@ if BpodSystem.Data.nTrials>1
         else
             fprintf('Error:SendFigureTo:Mail could not be sent to %s.\n',MailAddress);
         end
+                    
     end
-    
-    %% copy data to server
-    if copydata
-        try
-            %CHANGE THIS TO 'homes\YOUR-ACCOUNT-NAME' OR to 'SHARED-FOLDER-NAME'
-            %examples: user = 'homes\torben'; % user name on server
-            %          user = 'confidence';   % shared folder
-            [~,subject] = fileparts(fileparts(fileparts(fileparts(BpodSystem.DataPath))));
-            remotepath = fullfile('Z:','BpodData',subject,BpodSystem.CurrentProtocolName);
-            
-            if ~isdir(fullfile(remotepath,'Session Data'))
-                mkdir(fullfile(remotepath,'Session Data'));
-            end
-            if ~isdir(fullfile(remotepath,'Session Settings'))
-                mkdir(fullfile(remotepath,'Session Settings'));
-            end
-            if ~isdir(fullfile(remotepath,'Session Figures'))
-                mkdir(fullfile(remotepath,'Session Figures'));
-            end
-            if ~isdir(fullfile(remotepath,'Session Stimuli'))
-                mkdir(fullfile(remotepath,'Session Stimuli'));
-            end
+catch
+    if evernote
+        load('MailSettings.mat')
+        MailAddress = MailSettings.MailTo; % 'bosc274.b4f75e1@m.evernote.com';
+        
+        % Note informations:
+        titstr=strsplit(TitleString,'_');
+        note = sprintf('%s %s %s:',titstr{3},titstr{4},titstr{5});%date year session
+        note = sprintf('%s\nNo information available about this session\n',note);
 
-            save(fullfile(remotepath,'Session Data',[TitleString, '.mat']),'SessionData');
-            [~,filename,~]=fileparts(BpodSystem.SettingsPath);
-            newfilename=[filename '_' datestr(now,'yyyy-mm-dd') '.mat'];
-            copyfile(BpodSystem.SettingsPath,fullfile(remotepath,'Session Settings',newfilename));
-            copyfile(fullfile(FigureFolder,[FigureName '.png']),fullfile(remotepath,'Session Figures',[FigureName '.png']));
-            copyfile(fullfile(FigureFolder,[FigureName02 '.png']),fullfile(remotepath,'Session Figures',[FigureName02 '.png']));
-            copyfile(BpodSystem.Data.Custom.StimulusPath,fullfile(remotepath,'Session Stimuli',FigureName));
-                        fprintf('Files successfully copied to server!\n');
-
-        catch
-            fprintf('Error copying data to server. Files not copied!\n');
+        Subject = strcat(titstr{1}, ' @ ', titstr{2}, ' +');%subject protocol
+        Body = note;
+        
+        sent = SendMyMail(MailSettings,MailSettings.MailTo,Subject,Body); % (MailSettings,MailAddress,Subject,Body,Attachment);
+        if sent
+            fprintf('"%s" sent to %s.\n',Subject,MailAddress);
+        else
+            fprintf('Error:SendFigureTo:Mail could not be sent to %s.\n',MailAddress);
         end
     end
 end
 
+%% copy data to server
+if copydata
+    try
+        %CHANGE THIS TO 'homes\YOUR-ACCOUNT-NAME' OR to 'SHARED-FOLDER-NAME'
+        %examples: user = 'homes\torben'; % user name on server
+        %          user = 'confidence';   % shared folder
+        [~,subject] = fileparts(fileparts(fileparts(fileparts(BpodSystem.DataPath))));
+        remotepath = fullfile('Z:','BpodData',subject,BpodSystem.CurrentProtocolName);
+        
+        if ~isdir(fullfile(remotepath,'Session Data'))
+            mkdir(fullfile(remotepath,'Session Data'));
+        end
+        if ~isdir(fullfile(remotepath,'Session Settings'))
+            mkdir(fullfile(remotepath,'Session Settings'));
+        end
+        if ~isdir(fullfile(remotepath,'Session Figures'))
+            mkdir(fullfile(remotepath,'Session Figures'));
+        end
+        if ~isdir(fullfile(remotepath,'Session Stimuli'))
+            mkdir(fullfile(remotepath,'Session Stimuli'));
+        end
+        
+        save(fullfile(remotepath,'Session Data',[TitleString, '.mat']),'SessionData');
+        [~,filename,~]=fileparts(BpodSystem.SettingsPath);
+        newfilename=[filename '_' datestr(now,'yyyy-mm-dd') '.mat'];
+        copyfile(BpodSystem.SettingsPath,fullfile(remotepath,'Session Settings',newfilename));
+        copyfile(fullfile(FigureFolder,[FigureName '.png']),fullfile(remotepath,'Session Figures',[FigureName '.png']));
+        copyfile(fullfile(FigureFolder,[FigureName02 '.png']),fullfile(remotepath,'Session Figures',[FigureName02 '.png']));
+        copyfile(BpodSystem.Data.Custom.StimulusPath,fullfile(remotepath,'Session Stimuli',FigureName));
+        fprintf('Files successfully copied to server!\n');
+        
+    catch
+        fprintf('Error copying data to server. Files not copied!\n');
+    end
 end
+end
+
+
 
 function sent = SendMyMail(varargin)
 % sends mail from gmail account
